4670331f62274e9bc25bdd8267172e900a309d7b
Koni-Davies <estherkdavies@gmail.com>
2016-06-14 23:45:15 -0700

    Functioning New, Edit and Delete Methods

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -5,19 +5,39 @@ class ServicesController < ApplicationController
 	end
 
 	def new
-
+		@service = Service.new
+		@categories = Category.all
 	end
 	
 	def create
-
+		@service = Service.new(service_params)
+		
+		if @service.save
+			redirect_to :notice => "Your listing has been saved"
+		else 
+			@categories = Category.all
+			render :notice => "new"
+	end
+	end
+	
+	def service_params
+		params.require(:services).permit(:agency_name, :address_1, :address_2, :tel, :fax, :email, :website, :category_id, :author_id)
 	end
 	
 	def edit
-
+		@service = Service.find(params[:id])
+		@categories = Category.all
 	end
 	
 	def update
-
+		@service = Service.find(params[:id])
+		
+		if @service.update_attributes(service_param)
+			redirect_to :notice => "Your Listing has been updated", :id => @service
+		else
+			@categories = Category.all
+			render :notice => "edit"
+	end
 	end
 	
 	def show
@@ -26,8 +46,7 @@ class ServicesController < ApplicationController
 	end
 	
 	def destroy
-
+		Service.find(params[:id]).destroy
+		redirect_to :notice => 'index'
 	end
 end
-
-
