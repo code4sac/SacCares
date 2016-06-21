f96033fdbcc287535f44282a45a89a38fa8ddb80
Koni-Davies <estherkdavies@gmail.com>
2016-06-20 22:49:04 -0700

    Configured Admin Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,35 +1,33 @@
 ActiveAdmin.register_page "Dashboard" do
-
-  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
-
+menu :priority => 1
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Recent Agency Listings" do
+          table_for Service.order("created_at desc").limit(5) do
+            column :agency_name do |service|
+              link_to service.agency_name, [:admin, service]
+            end
+            column :created_at
+          end
+          strong { link_to "View all Agency Listings", admin_services_path }
+		strong { link_to "View all Category Listings", admin_categories_path }
+		 end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
 
-	
+      #column do
+        #panel "Recent Category Listings" do
+          #table_for Category.order("created_at desc").limit(5) do
+            #column :name do |category|
+              #link_to category.name, [:admin, category]
+            #end
+            #column :created_at
+          #end
+          #strong { link_to "View all Category Listings", admin_categories_path }		  
+		 #end
+      #end
+    #end
+  
