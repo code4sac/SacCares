730ba7f21c1163a61cff7d66abaa6a92816c7b1e
Koni-Davies <estherkdavies@gmail.com>
2016-06-26 15:24:17 -0700

    cleaned css sheet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :all_categories
+  helper_method :all_services
+    
+  def all_categories
+    @categories = Category.all
+	end
+	
+  def all_services
+    @services = Service.all
+	end
 end
